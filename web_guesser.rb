c11ab00f8906c12924b049d07e8ed84ed0262ea8
markusgustavsson <markusgustavsson21@gmail.com>
2016-10-16 19:40:25 +1300

    moved method out of get

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,6 +1,21 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+number = rand(99)
+
+def check_guess(x, y)
+ if x.to_i > y.to_i
+  "guess is too high"
+  elsif y.to_i > x.to_i
+   "guess is too low"
+  else 
+   "correct"
+  end
+end
+
+
 get '/' do
-  "The SECRET NUMBER is #{rand(99)}"
+guess = params["guess"].to_i
+message = check_guess(guess, number)
+  erb :index, :locals => {:number => number, :message => message}
 end
